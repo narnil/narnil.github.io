11c7a962f4b0096fbba25c9d5c940b81c93a74a1
narnil <narnil@users.noreply.github.com>
2023-04-09 10:51:41 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+async function connectToDevice() {
+  try {
+    const device = await navigator.bluetooth.requestDevice({
+      filters: [{ namePrefix: "Polar OH1" }],
+      optionalServices: ["heart_rate"]
+    });
+    const server = await device.gatt.connect();
+    const service = await server.getPrimaryService("heart_rate");
+    const characteristic = await service.getCharacteristic("heart_rate_measurement");
+    characteristic.addEventListener("characteristicvaluechanged", handleData);
+    await characteristic.startNotifications();
+  } catch (error) {
+    const connectButton = document.getElementById('connectButton');
+    connectButton.textContent = 'Connect';
+    connectButton.disabled = false;
+    console.error(error);
+  }
+}
+
+function handleData(event) {
+  const value = event.target.value;
+  const flags = value.getUint8(0);
+  const rate16Bits = flags & 0x1;
+  const result = {};
+  let index = 1;
+  if (rate16Bits) {
+      result.heartRate = value.getUint16(index, true);
+      index += 2;
+  } else {
+      result.heartRate = value.getUint8(index);
+      index += 1;
+  }
+
+  const heartRateDisplay = document.getElementById('heartRateDisplay');
+  heartRateDisplay.textContent = result.heartRate;
+
+  heartRate = result.heartRate;
+  console.log("Heart rate:", result.heartRate);
+}
+
+function checkHeartRate() {
+  if (heartRate < range.value.lower) {
+    alertLow.play();
+    console.log(`Heart rate is below minimum (${range.value.lower})`);
+  } else if (heartRate > range.value.upper) {
+    alertHigh.play();
+    console.log(`Heart rate is above maximum (${range.value.upper})`);
+  }
+}
+
+const connectButton = document.getElementById("connectButton");
+connectButton.addEventListener("click", async function() {
+    connectButton.disabled = true;
+    connectButton.textContent = 'Connecting';
+    await connectToDevice();
+    connectButton.textContent = 'Connected';
+    setInterval(checkHeartRate, 5000);
+});
+
+const volumeSlider = document.querySelector("#volumeSlider");
+volumeSlider.addEventListener('ionChange', function() {
+  const volume = volumeSlider.value;
+  alertLow.volume = volume/100;
+  alertHigh.volume = volume/100;
+});
+
+let heartRate = 80;
+const alertLow = new Audio('assets/low.mp3');
+const alertHigh = new Audio('assets/high.mp3');
+
+const rangeDisplay = document.querySelector("#rangeDisplay");
+const range = document.querySelector("#heartRateSlider");
+range.value = {
+  lower: 60,
+  upper: 140,
+};
+rangeDisplay.textContent = `Min rate: ${range.value.lower} / Max rate: ${range.value.upper}`;
+
+range.addEventListener('ionChange', function() {
+  rangeDisplay.textContent = `Min rate: ${range.value.lower} / Max rate: ${range.value.upper}`;
+});
